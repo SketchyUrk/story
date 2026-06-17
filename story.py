08150635200265c075eba0887e65b5b1546efcbc
Stelios <steliostamsios05@gmail.com>
2026-06-17 13:59:37 +0300

    commandcontext

---
+++ story.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+def startGame():
+    print("""Commands:
+          Interact with an item with 'USE <item>'
+          Look around by using 'look'
+          Move around using 'goto <location>'
+          You can view these commands anytime with 'help'""")
+    
